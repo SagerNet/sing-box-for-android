4e4b01b7b92b4ec6c18b15660bbd69658d02e8c9
世界 <i@sekai.icu>
2024-02-11 20:32:48 +0800

    Rename `service.Sleep()` to `Pause()`

--- app/src/main/java/io/nekohasekai/sfa/bg/BoxService.kt
+++ app/src/main/java/io/nekohasekai/sfa/bg/BoxService.kt
@@ -221,7 +221,7 @@ class BoxService(
     @RequiresApi(Build.VERSION_CODES.M)
     private fun serviceUpdateIdleMode() {
         if (Application.powerManager.isDeviceIdleMode) {
-            boxService?.sleep()
+            boxService?.pause()
         } else {
             boxService?.wake()
         }
