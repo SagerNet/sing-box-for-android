7d2c4cdba308c3e21cbd34e9d71890950ec4368b
邓超 <2325690622@qq.com>
2024-09-15 12:46:22 +0800

    Prevent multiple error dialogs shows up at the same time

--- app/src/main/java/io/nekohasekai/sfa/ktx/Dialogs.kt
+++ app/src/main/java/io/nekohasekai/sfa/ktx/Dialogs.kt
@@ -1,6 +1,7 @@
 package io.nekohasekai.sfa.ktx
 
 import android.content.Context
+import android.content.DialogInterface
 import androidx.annotation.StringRes
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.nekohasekai.sfa.R
@@ -13,12 +14,26 @@ fun Context.errorDialogBuilder(@StringRes messageId: Int): MaterialAlertDialogBu
 }
 
 fun Context.errorDialogBuilder(message: String): MaterialAlertDialogBuilder {
+    return errorDialogBuilder(message, null)
+}
+
+fun Context.errorDialogBuilder(
+    message: String,
+    listener: DialogInterface.OnClickListener?
+): MaterialAlertDialogBuilder {
     return MaterialAlertDialogBuilder(this)
         .setTitle(R.string.error_title)
         .setMessage(message)
-        .setPositiveButton(android.R.string.ok, null)
+        .setPositiveButton(android.R.string.ok, listener)
 }
 
 fun Context.errorDialogBuilder(exception: Throwable): MaterialAlertDialogBuilder {
-    return errorDialogBuilder(exception.localizedMessage ?: exception.toString())
+    return errorDialogBuilder(exception, null)
+}
+
+fun Context.errorDialogBuilder(
+    exception: Throwable,
+    listener: DialogInterface.OnClickListener?
+): MaterialAlertDialogBuilder {
+    return errorDialogBuilder(exception.localizedMessage ?: exception.toString(), listener)
 }
