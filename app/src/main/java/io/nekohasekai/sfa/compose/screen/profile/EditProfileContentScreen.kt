d69a6b38b9105746400374a47ce4a13c7e946ce6
世界 <i@sekai.icu>
2025-12-18 19:35:22 +0800

    Fix editor toolbar being hidden behind soft keyboard

--- app/src/main/java/io/nekohasekai/sfa/compose/screen/profile/EditProfileContentScreen.kt
+++ app/src/main/java/io/nekohasekai/sfa/compose/screen/profile/EditProfileContentScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -478,7 +479,8 @@ fun EditProfileContentScreen(
                         modifier =
                             Modifier
                                 .align(Alignment.BottomCenter)
-                                .fillMaxWidth(),
+                                .fillMaxWidth()
+                                .imePadding(),
                     ) {
                         // Configuration error banner (appears above the symbol bar)
                         AnimatedVisibility(
