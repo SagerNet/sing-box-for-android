f653f891fc9a35aaeca98cb5a349daaea92164e4
世界 <i@sekai.icu>
2026-07-21 14:35:53 +0800

    Update locale usage

--- app/src/main/java/io/nekohasekai/sfa/compose/MainActivity.kt
+++ app/src/main/java/io/nekohasekai/sfa/compose/MainActivity.kt
@@ -8,6 +8,7 @@ import android.net.Uri
 import android.net.VpnService
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -76,6 +77,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.os.ConfigurationCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -207,6 +209,13 @@ class MainActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ConfigurationCompat.getLocales(resources.configuration)[0]?.let { locale ->
+            runCatching {
+                Libbox.setLocale(locale.toLanguageTag())
+            }.onFailure {
+                Log.d("MainActivity", "set locale: ${it.message}")
+            }
+        }
         enableEdgeToEdge()
 
         connection.reconnect()
